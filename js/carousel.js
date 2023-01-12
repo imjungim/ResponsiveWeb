12a938ab6ddae9763d4e1c28612150b29a6486a0
imjungim <jungim4201@gmail.com>
2023-01-12 21:42:40 +0900

    🚧 Import tiny-slider.js

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+const slider = tns({
+  container: '.carousel-slider .slider-list',
+  controls: false,
+  navContainer: '.carousel-thumbnail .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
